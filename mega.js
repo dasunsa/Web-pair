634656ba541f9273335d62bfb8b649ce23d09367
dasunsa <sankalanadasun453@gmail.com>
2025-07-11 14:08:42 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'sankalanadasun453@gmail.com
-',
+    email: 'sankalanadasun453@gmail.com',
+
     password: 'FJtzubzQU2Q:DKE',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
